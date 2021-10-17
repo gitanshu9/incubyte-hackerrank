a1c3df2f8d819339d3be4b1d1c132f6099a30da7
Anshuman <panshu233@gmail.com>
2021-10-17 09:47:41 +0530

    for adding two numbers

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -1,9 +1,37 @@
 package calculator;
 
-class StringCalculator {
+public class StringCalculator {
 
-    public int add(String input) {
-        return 0;
+	public static int add(String text){
+		if(text.equals("")){
+			return 0;
+		}
+		else{
+			String delimiter = ",";
+			String numList[] = splitNumbers(text, delimiter); 
+			return sum(numList);
+			}
+			
+			
+		}
+
+	private static int toInt(String number){
+		return Integer.parseInt(number);
+	}
+
+	private static String[] splitNumbers(String numbers, String divider){
+	    return numbers.split(divider);
+	}
+
+	private static int sum(String[] numbers){
+ 	    int total = 0;
+
+        for(String number : numbers){
+        	if(toInt(number) < 1000)
+		    	total += toInt(number);
+		}
+
+
+		return total;
     }
-
 }
--- src/test/java/calculator/StringCalculatorShould.java
+++ src/test/java/calculator/StringCalculatorShould.java
@@ -1,20 +1,29 @@
 package calculator;
 
-import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
 
-class StringCalculatorShould {
+public class StringCalculatorShould {
 
-    @Test
-    void empty_string_should_return_0() {
-        StringCalculator stringCalculator = new StringCalculator();
-        assertEquals(0, stringCalculator.add(""));
+	public static void main(String args[]) {
+      org.junit.runner.JUnitCore.main("is.ru.stringcalculator.CalculatorTest");
     }
 
-    @Test
-    void string_with_single_number_should_return_number_as_int() {
-        StringCalculator stringCalculator = new StringCalculator();
-        assertEquals(1, stringCalculator.add("1"));
-    }
+	@Test
+	public void testEmptyString() {
+		assertEquals(0, StringCalculator.add(""));
+	}
+
+	@Test
+	public void testOneNumber() {
+		assertEquals(1, StringCalculator.add("1"));
+	}
+
+	@Test
+	public void testTwoNumbers(){
+		assertEquals(3, StringCalculator.add("1,2"));
+	}
+
+	
 }
